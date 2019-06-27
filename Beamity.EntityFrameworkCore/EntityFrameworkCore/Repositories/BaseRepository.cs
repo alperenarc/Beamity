084c43c6e5ca2ddd5baec68f0bd326e1f832acba
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-06-27 11:40:51 +0300

    Base Repository and Interface Added

---
+++ Beamity.EntityFrameworkCore/EntityFrameworkCore/Repositories/BaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beamity.EntityFrameworkCore.EntityFrameworkCore.Repositories
+{
+    class BaseRepository
+    {
+    }
+}
