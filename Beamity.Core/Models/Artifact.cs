6b2207b1dd779df84c9d546e850f45739f75f6da
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-06-27 10:16:34 +0300

    Artifact,Content,Room,Relation
    Artifact,Content,Room,Relation added

---
+++ Beamity.Core/Models/Artifact.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Beamity.Core.Models
+{
+    public class Artifact : EntityBase
+    {
+        [Required(ErrorMessage = "Name Required")]
+        [MaxLength(47)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Main Image Required")]
+        [Display(Name = "Main Image")]
+        public string MainImageURL { get; set; }
+
+        /*Froign Keys*/
+        public int RelationId { get; set; }
+        public Relation Relation { get; set; }
+
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+    }
+}
---
+++ Beamity.Core/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Beamity.Core.Models
+{
+    public class Room
+    {
+        [Required(ErrorMessage = "Name Required")]
+        [MaxLength(47)]
+        public string Name { get; set; }
+
+        /*Froign Keys*/
+        public int FlootId { get; set; }
+        public Floot Floor{ get; set; }
+    }
+}
