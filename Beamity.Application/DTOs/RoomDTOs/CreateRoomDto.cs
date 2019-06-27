71fbcb33dc85e354fdcfc8a9fe5cd6ad0a579ce0
Alperen ARICI <alparicieren@gmail.com>
2019-06-27 17:59:49 +0300

    CreateRoomDTO added

--- Beamity.Application/DTOs/RoomDTOs/CreateRoomDto.cs
+++ Beamity.Application/DTOs/RoomDTOs/CreateRoomDto.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Beamity.Application.DTOs.RoomDTOs
 {
     class CreateRoomDTO
     {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Guid FloorId { get; set; }
+
     }
 }
