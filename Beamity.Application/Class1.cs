8d6c9b1544316f9394a01ad6e3806c17af51d7a7
Alperen ARICI <alparicieren@gmail.com>
2019-06-27 09:22:24 +0300

    Interface Added

--- Beamity.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Beamity.Application
-{
-    public class Class1
-    {
-    }
-}
---
+++ Beamity.Core/Interfaces/IEntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Beamity.Core.Interfaces
+{
+    public interface IEntityBase
+    {
+        
+        Guid ID { get; set; }
+        bool IsActive { get; set; }
+        DateTime CreatedTime { get; set; }
+
+    }
+}
