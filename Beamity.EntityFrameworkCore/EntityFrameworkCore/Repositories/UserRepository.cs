3ad317e49791e261d21d30ccfb95f63af1eaf824
Alperen ARICI <alparicieren@gmail.com>
2019-06-27 12:10:26 +0300

    User Repository Added
    Base repository fixed

---
+++ Beamity.EntityFrameworkCore/EntityFrameworkCore/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Beamity.Core.Models;
+using Beamity.EntityFrameworkCore.EntityFrameworkCore.Contexts;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beamity.EntityFrameworkCore.EntityFrameworkCore.Repositories
+{
+    public class UserRepository: BaseRepository<User>
+    {
+        public UserRepository(BeamityDbContext context) : base(context)
+        {
+        }
+    }
+}
