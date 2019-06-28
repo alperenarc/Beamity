68c879213034771d8f07ccd8ec3b2a5906ca9e6f
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-06-28 11:02:57 +0300

    Update IService.cs

--- Beamity.Application/Service/IService.cs
+++ Beamity.Application/Service/IService.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Beamity.Application.Service
 {
-    public interface IService
+    public interface IService<TModel>
     {
+        Task<List<TModel>> GetAll();
+        Task<TModel> GetById(Guid id);
+        TModel Create(TModel model);
+        Task Update(TModel model);
+        void Delete(TModel model);
+        void Delete(Guid id);
     }
 }
