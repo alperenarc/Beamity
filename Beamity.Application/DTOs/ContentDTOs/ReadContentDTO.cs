889758c0f8a57d618557bf257bcd274a3480c40b
Alperen ARICI <alparicieren@gmail.com>
2019-06-28 09:36:01 +0300

    Updated DTOS

--- Beamity.Application/DTOs/ContentDTOs/ReadContentDTO.cs
+++ Beamity.Application/DTOs/ContentDTOs/ReadContentDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Beamity.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -15,6 +16,7 @@ namespace Beamity.Application.DTOs.ContentDTOs
         public string AudioURL { get; set; }
         public string Text { get; set; }
         public bool IsHomePage { get; set; }
-        
+
+        public Relation Relation { get; set; }
     }
 }
