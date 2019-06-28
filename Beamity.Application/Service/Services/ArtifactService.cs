376767c3ac045ddd8d2ad3f2e30c4bf48b404f47
Alperen ARICI <alparicieren@gmail.com>
2019-06-28 11:08:18 +0300

    Artifact Service and User service added

---
+++ Beamity.Application/Service/Services/ArtifactService.cs
@@ -0,0 +1,16 @@
+﻿using Beamity.EntityFrameworkCore.EntityFrameworkCore.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beamity.Application.Service.Services
+{
+    public class ArtifactService : IService
+    {
+        private readonly ArtifactRepository _repository;
+        public ArtifactService(ArtifactRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
