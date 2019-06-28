355f1c486b1e694357772d410a8b37a3a3cfae6e
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-06-28 09:34:15 +0300

    DTO added

---
+++ Beamity.Application/DTOs/BuildingDTOs/DeleteBuildingDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beamity.Application.DTOs.BuildingDTOs
+{
+    public class DeleteBuildingDTO : BaseDTO<Guid>
+    {
+    }
+}
---
+++ Beamity.Application/DTOs/BuildingDTOs/UpdateBuildingDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beamity.Application.DTOs.BuildingDTOs
+{
+    public class UpdateBuildingDTO 
+    {
+        public string Name { get; set; }
+        public int LocationId { get; set; }
+    }
+}
