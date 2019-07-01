68c2369998916ac774ee6edf896d645ff3fa86e5
Alperen ARICI <alparicieren@gmail.com>
2019-07-01 15:50:53 +0300

    Room Relation controller completed

--- Beamity.API/Controllers/RelationController.cs
+++ Beamity.API/Controllers/RelationController.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Beamity.Application.DTOs;
+using Beamity.Application.DTOs.BeaconDTOs;
+using Beamity.Application.DTOs.ContentDTOs;
+using Beamity.Application.DTOs.RelationDTO;
 using Beamity.Application.Service.IServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Beamity.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class RelationController : ControllerBase
     {
@@ -16,6 +20,62 @@ namespace Beamity.API.Controllers
         public RelationController(IRelationService relationService)
         {
             _relationService = relationService;
+        }
+        [HttpGet]
+        public List<ReadRelationDTO> GetAllRelations()
+        {
+            var relations = _relationService.GetAllRelations();
+            return relations;
+        }
+        [HttpGet]
+        public ReadRelationDTO GetRelation(EntityDTO Relation)
+        {
+
+            var relation = _relationService.GetRelation(Relation);
+            return relation;
+
+        }
+        [HttpGet]
+        public ReadContentDTO GetContentWithBeacon(GetContentWithBeaconDTO input)
+        {
+            var content = _relationService.GetContentWithBeacon(input);
+            return content;
+        }
+        [HttpPost]
+        public void CreateRelation(CreateRelationDTO input)
+        {
+            try
+            {
+                _relationService.CreateRelation(input);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpDelete]
+        public void DeleteRelationDTO(DeleteRelationDTO input)
+        {
+            try
+            {
+                _relationService.DeleteRelationDTO(input);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpPut]
+        public void UpdateRelation(UpdateRelationDTO input)
+        {
+            try
+            {
+                _relationService.UpdateRelation(input);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
--- Beamity.API/Controllers/RoomController.cs
+++ Beamity.API/Controllers/RoomController.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Beamity.Application.DTOs;
+using Beamity.Application.DTOs.RoomDTOs;
 using Beamity.Application.Service.IServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Beamity.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class RoomController : ControllerBase
     {
@@ -16,6 +18,61 @@ namespace Beamity.API.Controllers
         public RoomController(IRoomService roomService)
         {
             _roomService = roomService;
+        }
+        [HttpPost]
+        public IActionResult CreateRoom(CreateRoomDTO room)
+        {
+            try
+            {
+                _roomService.CreateRoom(room);
+            }
+            catch (Exception)
+            {
+                return BadRequest("An error occurred during the creating process. Please try again !");
+            }
+            return Ok("The process is success");
+        }
+        [HttpPut]
+        public IActionResult UpdateRoom(UpdateRoomDTO room)
+        {
+            try
+            {
+                _roomService.UpdateRoom(room);
+            }
+            catch (Exception)
+            {
+                return BadRequest("An error occurred during the editing process. Please try again !");
+            }
+            return Ok("The process is success");
+        }
+        [HttpDelete]
+        public IActionResult DeleteRoom(DeleteRoomDTO room)
+        {
+            try
+            {
+                _roomService.DeleteRoom(room);
+            }
+            catch (Exception)
+            {
+                return BadRequest("An error occurred during the deleting process. Please try again !");
+            }
+            return Ok("The process is success");
+        }
+        [HttpGet]
+        public ReadRoomDTO getRoom(EntityDTO id)
+        {
+            var room = _roomService.GetRoom(id);
+            return room;
+        }
+        [HttpGet]
+        public List<ReadRoomDTO> GetAllRooms()
+        {
+            return _roomService.GetAllRooms();
+        }
+        [HttpGet]
+        public async Task<List<ReadRoomDTO>> GetRoomsOnFloor(EntityDTO room)
+        {
+            return await _roomService.GetRoomsOnFloor(room);
         }
     }
 }
