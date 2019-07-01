1ad82abe2bf86be5e2eab67ecd797e8305809264
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-07-01 08:38:59 +0300

    Service and interface Update

--- Beamity.Application/Service/Services/LocationService.cs
+++ Beamity.Application/Service/Services/LocationService.cs
@@ -1,4 +1,9 @@
-﻿using Beamity.EntityFrameworkCore.EntityFrameworkCore.Repositories;
+﻿using AutoMapper;
+using Beamity.Application.DTOs;
+using Beamity.Application.DTOs.LocationDTOs;
+using Beamity.Application.Service.IServices;
+using Beamity.Core.Models;
+using Beamity.EntityFrameworkCore.EntityFrameworkCore.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +13,41 @@ namespace Beamity.Application.Service.Services
     public class LocationService : ILocationService
     {
         private readonly LocationRepository _repository;
-        public LocationService(LocationRepository repository)
+        private readonly IMapper _mapper;
+        public LocationService(LocationRepository repository, IMapper mapper)
         {
+            _mapper = mapper;
             _repository = repository;
+        }
+
+        public void CreateLocation(CreateLocationDTO input)
+        {
+            var location = _mapper.Map<Location>(input);
+            _repository.Create(location);
+        }
+
+        public void DeleteLocation(DeleteLocationDTO input)
+        {
+            _repository.Delete(input.Id);
+        }
+
+        public List<ReadLocationDTO> GetAllLocation()
+        {
+            var locations = _repository.GetAll();
+            var result = _mapper.Map<List<ReadLocationDTO>>(locations);
+            return result;
+        }
+
+        public ReadLocationDTO GetLocation(EntityDTO input)
+        {
+            var location = _repository.GetById(input.Id);
+            return _mapper.Map<ReadLocationDTO>(location);
+        }
+
+        public void UpdateLocation(UpdateLocationDTO input)
+        {
+            var location = _mapper.Map<Location>(input);
+            _repository.Update(location);
         }
     }
 }
--- Beamity.Application/Service/Services/ProjectService.cs
+++ Beamity.Application/Service/Services/ProjectService.cs
@@ -1,4 +1,9 @@
-﻿using Beamity.EntityFrameworkCore.EntityFrameworkCore.Repositories;
+﻿using AutoMapper;
+using Beamity.Application.DTOs;
+using Beamity.Application.DTOs.ProjectDTOs;
+using Beamity.Application.Service.IServices;
+using Beamity.Core.Models;
+using Beamity.EntityFrameworkCore.EntityFrameworkCore.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +13,41 @@ namespace Beamity.Application.Service.Services
     public class ProjectService : IProjectService
     {
         private readonly ProjectRepository _repository;
-        public ProjectService(ProjectRepository repository)
+        private readonly IMapper _mapper;
+        public ProjectService(ProjectRepository repository, IMapper mapper)
         {
+            _mapper = mapper;
             _repository = repository;
+        }
+
+        public void CreateProcet(CreateProjectDTO input)
+        {
+            var project = _mapper.Map<Project>(input);
+            _repository.Create(project);
+        }
+
+        public void DeleteProject(DeleteProjectDTO input)
+        {
+            _repository.Delete(input.Id);
+        }
+
+        public List<ReadProjectDTO> GetAllProject()
+        {
+            var projects = _repository.GetAll();
+            var result = _mapper.Map<List<ReadProjectDTO>>(projects);
+            return result;
+        }
+
+        public ReadProjectDTO GetProject(EntityDTO input)
+        {
+            var project = _repository.GetById(input.Id);
+            return _mapper.Map<ReadProjectDTO>(project);
+        }
+
+        public void UpdateProject(UpdateProjectDTO input)
+        {
+            var project = _mapper.Map<Project>(input);
+            _repository.Update(project);
         }
     }
 }
