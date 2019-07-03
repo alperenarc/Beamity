12be8492277da16d682394b701a078fab76ebf52
Alperen ARICI <alparicieren@gmail.com>
2019-07-03 09:15:10 +0300

    AutoMapping completed

--- Beamity.Web/Startup.cs
+++ Beamity.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
