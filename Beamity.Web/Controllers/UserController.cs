abd4811c91f1820ef9fe9f1d4fe3d2712cb45e6e
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-07-03 10:12:35 +0300

    web changes

---
+++ Beamity.Web/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beamity.Web.Controllers
+{
+    public class UserController : Controller
+    { 
+        
+        public ActionResult Profile(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Register()
+        {
+            return View();
+        }
+        
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
