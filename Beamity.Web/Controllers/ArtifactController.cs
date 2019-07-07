5519d67e7899f4621e0b040d46ab7f5aa4470a03
Alperen ARICI <alparicieren@gmail.com>
2019-07-07 11:26:04 +0300

    a number of changes were made.

--- Beamity.Web/Controllers/ArtifactController.cs
+++ Beamity.Web/Controllers/ArtifactController.cs
@@ -25,7 +25,7 @@ namespace Beamity.Web.Controllers
             //guid = Guid.NewGuid().ToString();
 
 
-            //IEnumerable <ReadArtifactDTO> artifacts = null;
+            //IEnumerable<ReadArtifactDTO> artifacts = null;
 
             //using (var client = new HttpClient())
             //{
@@ -47,22 +47,18 @@ namespace Beamity.Web.Controllers
                 return Content("Argument null");
             if (inputModel.File == null || inputModel.File.Length == 0)
                 return Content("file not selected");
+
             var blobName = inputModel.File.GetFilename();
             var fileStream = await inputModel.File.GetFileStream();
+
+
             if (!string.IsNullOrEmpty(inputModel.Folder))
                 blobName = string.Format(@"{0}\{1}", inputModel.Folder, blobName);
 
-
-            var ext = blobName.Split('.');
-            var e = ext[ext.Length - 1];
-
-            string guid = Guid.NewGuid().ToString();
-            blobName = guid + "." + e;
             await _blobStorage.UploadAsync(blobName, fileStream);
-            string url = "https://testblobkayten.blob.core.windows.net/blobcontainer/" + blobName + "?sv=2018-03-28&ss=bqtf&srt=sco&sp=rwdlacup&se=2019-07-05T16:28:48Z&sig=E8YuTSKxReDDt8%2F4fvHKiMtsNmS8X8KfOXuax9Ut5I4%3D&_=1562317007361";
 
-
-            return RedirectToAction("Index");
+            
+            return ViewBag.Mesaj = "Mesaj";
         }
 
     }
