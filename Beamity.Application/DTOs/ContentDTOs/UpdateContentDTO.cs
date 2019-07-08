bf4952ae45d095f58679c5e9af03373a3143af50
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-07-08 17:56:30 +0300

    web update

--- Beamity.Application/DTOs/ContentDTOs/UpdateContentDTO.cs
+++ Beamity.Application/DTOs/ContentDTOs/UpdateContentDTO.cs
@@ -19,6 +19,5 @@ namespace Beamity.Application.DTOs.ContentDTOs
         public string Text { get; set; }
         public bool IsHomePage { get; set; }
 
-        public Guid RelationID { get; set; }
     }
 }
--- Beamity.Web/Controllers/ContentController.cs
+++ Beamity.Web/Controllers/ContentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Beamity.Application.DTOs;
 using Beamity.Application.DTOs.ContentDTOs;
 using Beamity.Application.Service.IServices;
 using Beamity.Web.Blob;
@@ -52,6 +53,53 @@ namespace Beamity.Web.Controllers
                     Text = input.Text
                 };
                 _contentService.CrateContent(data);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+        public IActionResult Update(EntityDTO input)
+        {
+            var content = _contentService.GetContent(input);
+            UpdateContentViewModel data = new UpdateContentViewModel()
+            {
+                Name = content.Name,
+                Description = content.Description,
+                Title = content.Title,
+                
+                IsHomePage = content.IsHomePage,
+                Text = content.Text,
+                
+            };
+            return View(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateContentViewModel input)
+        {
+            try
+            {
+                string mainImageUrl = await _blobManager.UploadImageAsBlob(input.MainImage);
+                string slideImageUrl = await _blobManager.UploadImageAsBlob(input.SlideImage);
+                string videoUrl = await _blobManager.UploadImageAsBlob(input.Video);
+                string audioUrl = await _blobManager.UploadImageAsBlob(input.Audio);
+                UpdateContentDTO data = new UpdateContentDTO()
+                {
+                    Name = input.Name,
+                    Description = input.Description,
+                    Title = input.Title,
+                    MainImageURL = mainImageUrl,
+                    SlideImageURL = slideImageUrl,
+                    VideoURL = videoUrl,
+                    AudioURL = audioUrl,
+                    IsHomePage = input.IsHomePage,
+                    Text = input.Text,
+                    CreatedTime = DateTime.Now
+                };
+                _contentService.UpdateContent(data);
+                return RedirectToAction(nameof(Index));
             }
             catch (Exception e)
             {
