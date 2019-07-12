1e6c68869a35af677abf62519f15ffd0c730f2e0
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-07-12 11:07:39 +0300

    Analytice service added

---
+++ Beamity.Application/Service/Services/AnalyticService.cs
@@ -0,0 +1,11 @@
+﻿using Beamity.Application.Service.IServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beamity.Application.Service.Services
+{
+    public class AnalyticService : IAnalyticService
+    {
+    }
+}
