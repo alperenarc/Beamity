494d3cfcf70e177264a36bc1e881132c8c184b0d
Alperen ARICI <alparicieren@gmail.com>
2019-07-12 15:58:42 +0300

    Beamity Base Controller added

---
+++ Beamity.Web/Controllers/BeamityBaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beamity.Web.Controllers
+{
+    public class BeamityBaseController : Controller
+    {
+        public BeamityBaseController()
+        {
+            //if (HttpContext.Session.GetString("LocationId") == null)
+            //{
+            //    return RedirectToAction("ChooseLocation", "Location");
+            //}
+        }
+    }
+}
