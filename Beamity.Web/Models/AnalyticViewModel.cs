a7eff0749fcf5cbe3f531e6c3af77541b52a5a08
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-07-18 13:32:45 +0300

    dashborad update

---
+++ Beamity.Web/Models/AnalyticViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beamity.Web.Models
+{
+    public class AnalyticViewModel
+    {
+        public string Name { get; set; }
+        public int Hour { get; set; }
+        public int Count { get; set; }
+    }
+}
